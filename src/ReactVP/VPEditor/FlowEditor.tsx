ab43a8e8c8f55030ed03a1e830e05a5318d1fc95
Max-ChenFei <boxchenfei@gmail.com>
2024-09-20 19:43:42 +0200

    FitView after loading the graph

--- src/ReactVP/VPEditor/FlowEditor.tsx
+++ src/ReactVP/VPEditor/FlowEditor.tsx
@@ -36,6 +36,7 @@ export default function FlowEditor({
             'scene',
             new SceneActions(reactFlowInstance)
           );
+          reactFlowInstance.fitView();
         }}
         // for rendering
         nodeTypes={editorContext.nodeTypes}
